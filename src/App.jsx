481395c3f5b6e6ad690125f2442003e47c724810
DariaAleshina <ddaleshina@gmail.com>
2025-07-17 15:23:09 +0200

    Interval added - scores decrease over time

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,45 @@ import './App.css';
 const MAX_SCORE = 100;
 
 function App() {
-  const [hunger, setHunger] = useState(0);
+  const [fullness, setFullness] = useState(MAX_SCORE);
   const [happiness, setHappiness] = useState(MAX_SCORE);
   const [energy, setEnergy] = useState(MAX_SCORE);
   const [isGameOver, setIsGameOver] = useState(false);
 
+  useEffect(() => {
+    if (fullness === 0 || happiness === 0 || energy === 0) {
+      setIsGameOver(true);
+    }
+  }, [fullness, happiness, energy]);
+
+  // decrease stats over time
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFullness(f => Math.max(f - 5, 0));
+      setHappiness(h => Math.max(h - 3, 0));
+      setEnergy(e => Math.max(e - 2, 0));
+    }, 5000); // every 5 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
   function handleFeed() {
     console.log('clicked to FEED');
+    setFullness(f => Math.min(f + 15, MAX_SCORE));
   }
 
   function handlePlay() {
     console.log('clicked to Play');
+    setHappiness(h => Math.min(h + 15, MAX_SCORE));
   }
 
   function handleSleep() {
     console.log('clicked to SLEEP');
+    setEnergy(e => Math.min(e + 15, MAX_SCORE));
   }
 
   function handleReset() {
-    setHunger(0);
+    setFullness(MAX_SCORE);
     setHappiness(MAX_SCORE);
     setEnergy(MAX_SCORE);
   }
@@ -38,9 +58,9 @@ function App() {
         className="lg:w-3xl mr-auto ml-auto flex justify-center flex-col items-center gap-5
       "
       >
-        <StatBar hunger={hunger} happiness={happiness} energy={energy} />
+        <StatBar fullness={fullness} happiness={happiness} energy={energy} />
         <PetDisplay
-          hunger={hunger}
+          fullness={fullness}
           happiness={happiness}
           energy={energy}
           isGameOver={isGameOver}
--- src/components/PetDisplay.jsx
+++ src/components/PetDisplay.jsx
@@ -1,11 +1,51 @@
-import frenchieHappy from '../assets/frenchie-happy.png';
-import frenchieSad from '../assets/frenchie-sad.png';
-import frenchieSleepy from '../assets/frenchie-sleepy.png';
-import frenchieHungry from '../assets/frenchie-hungry.png';
-import frenchieNormal from '../assets/frenchie-normal.png';
+import happyImg from '../assets/frenchie-happy.png';
+import sadImg from '../assets/frenchie-sad.png';
+import sleepyImg from '../assets/frenchie-sleepy.png';
+import hungryImg from '../assets/frenchie-hungry.png';
+import normalImg from '../assets/frenchie-normal.png';
 
-export default function PetDisplay() {
-  let image = frenchieNormal;
+export default function PetDisplay({
+  fullness,
+  happiness,
+  energy,
+  isGameOver,
+}) {
+  let message = '';
+  let image;
 
-  return <img className="w-2xs" alt="Frenchie" src={image} />;
+  if (isGameOver) {
+    image = sadImg;
+  }
+
+  if (!isGameOver) {
+    if (happiness >= 80) {
+      message = 'happy';
+      image = happyImg;
+    }
+    if (happiness < 80) {
+      message = 'feels good';
+      image = normalImg;
+    }
+    if (happiness < 40) {
+      message = 'got sad';
+      image = sadImg;
+    }
+
+    if (energy < 40) {
+      message = 'sleeping';
+      image = sleepyImg;
+    }
+
+    if (fullness < 40) {
+      message = 'hungry';
+      image = hungryImg;
+    }
+  }
+
+  return (
+    <>
+      <img className="w-2xs" alt="Frenchie emotion" src={image} />
+      {isGameOver ? <p>Game is over</p> : <p>Your Frenchie is {message}</p>}
+    </>
+  );
 }
