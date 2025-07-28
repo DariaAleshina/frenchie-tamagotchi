1dc8803c5da2ec0e9447fadcad5440d834e32aec
DariaAleshina <ddaleshina@gmail.com>
2025-07-28 22:51:43 +0200

    toggleComp: attempt 1 to fix span moving left-right

--- src/components/ModeToggle.jsx
+++ src/components/ModeToggle.jsx
@@ -3,13 +3,13 @@ function ModeToggle({ isModeFast, onModeChange }) {
     <div className="text-sm flex border-2 justify-between items-center p-0.5">
       <div className="relative z-0 flex justify-between items-center gap-1">
         <span
-          className={`absolute bg-yellow-3 w-1/2 h-full ${
-            isModeFast ? 'right' : 'left'
-          }-0 z-1 transition-all duration-700`}
+          className={`absolute bg-yellow-3 w-1/2 h-full z-1 transition-all duration-700 ${
+            isModeFast ? 'right-0' : 'left-0'
+          }`}
           aria-hidden="true"
         />
         <button
-          className="relative px-1 py-0.5 z-2 focus:outline-none focus:text-yellow-dark hover:text-yellow-dark"
+          className="relative px-1 py-0.5 z-2 cursor-pointer focus:outline-none focus:text-yellow-dark hover:text-yellow-dark"
           onClick={() => onModeChange(false)}
           aria-pressed={!isModeFast}
           aria-label="Switch to slow mode"
@@ -17,7 +17,7 @@ function ModeToggle({ isModeFast, onModeChange }) {
           slow
         </button>
         <button
-          className="relative px-1 py-0.5 z-2 focus:outline-none focus:text-yellow-dark hover:text-yellow-dark"
+          className="relative px-1 py-0.5 z-2 cursor-pointer focus:outline-none focus:text-yellow-dark hover:text-yellow-dark"
           onClick={() => onModeChange(true)}
           aria-label="Switch to fast mode"
           aria-pressed={isModeFast}
