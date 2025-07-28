c5c65181fa864d390b63320bf2ec927c52b02e53
DariaAleshina <ddaleshina@gmail.com>
2025-07-28 20:09:49 +0200

    added mode-switch design & fixed H1 size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,10 @@ function Header() {
   return (
     <header>
       <div className="flex justify-between mb-5">
-        <nav>
+        <nav className="flex justify-center items-center">
           <NavLink linkToSection="game-rules">Game Rules</NavLink>
         </nav>
-        <div>
-          <button title="Toggle theme">🔥</button>
-        </div>
+        <SwitchMode />
       </div>
       <Heading />
     </header>
@@ -17,6 +15,22 @@ function Header() {
 }
 
 export default Header;
+
+function SwitchMode() {
+  return (
+    <div className="text-sm flex border-2 justify-between items-center p-0.5">
+      <div className="relative z-0 flex justify-between items-center gap-1">
+        <span className="absolute bg-yellow-3 w-1/2 h-full right-0 z-1 transition-all duration-700" />
+        <button className="relative px-1 py-0.5 z-2 focus:outline-none focus:inset-ring-3 focus:inset-ring-red-1">
+          slow
+        </button>
+        <button className="relative px-1 py-0.5 z-2 focus:outline-none focus:inset-ring-3 focus:inset-ring-red-1">
+          fast
+        </button>
+      </div>
+    </div>
+  );
+}
 
 function NavLink({ children, linkToSection }) {
   return (
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,7 +2,7 @@ import { gameTitle } from '../contentGameLogic';
 
 function Heading() {
   return (
-    <h1 className="text-3xl mb-5 md:text-6xl md:mb-5 text-center">
+    <h1 className="text-3xl mb-5 sm:text-4xl md:text-5xl lg:text-6xl md:mb-5 text-center">
       {gameTitle}
     </h1>
   );
