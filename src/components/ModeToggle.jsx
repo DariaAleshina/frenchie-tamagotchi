d371022f8d943456f42af495462d38e39cd09dc0
DariaAleshina <ddaleshina@gmail.com>
2025-07-29 11:41:09 +0200

    mode toggler: fix glitch on safari

--- src/components/ModeToggle.jsx
+++ src/components/ModeToggle.jsx
@@ -1,16 +1,16 @@
 function ModeToggle({ isModeFast, onModeChange }) {
   return (
     <div className="text-sm flex border-2 justify-between items-center p-0.5">
-      <div className="relative z-0 flex justify-between items-center gap-1">
+      <div className="relative z-0 flex justify-between items-center gap-1 overflow-hidden [transform-style:preserve-3d] [backface-visibility:hidden]">
         <span
-          className={`absolute bg-yellow-3 w-1/2 h-full z-1 transition-all duration-500 ${
+          className={`absolute bg-yellow-shadow w-1/2 h-full z-0 transition-transform duration-700 left-0 will-change-transform pointer-events-none mix-blend-multiply ${
             isModeFast ? 'translate-x-full' : 'translate-x-0'
           }
           `}
           aria-hidden="true"
         />
         <button
-          className="relative px-1 py-0.5 z-2 cursor-pointer focus:outline-none focus:text-yellow-dark hover:text-yellow-dark"
+          className="relative px-1 py-0.5 z-10 cursor-pointer focus:outline-none focus:text-yellow-dark hover:text-yellow-dark"
           onClick={() => onModeChange(false)}
           aria-pressed={!isModeFast}
           aria-label="Switch to slow mode"
@@ -18,7 +18,7 @@ function ModeToggle({ isModeFast, onModeChange }) {
           slow
         </button>
         <button
-          className="relative px-1 py-0.5 z-2 cursor-pointer focus:outline-none focus:text-yellow-dark hover:text-yellow-dark"
+          className="relative px-1 py-0.5 z-10 cursor-pointer focus:outline-none focus:text-yellow-dark hover:text-yellow-dark"
           onClick={() => onModeChange(true)}
           aria-label="Switch to fast mode"
           aria-pressed={isModeFast}
