d82b89c718436b2a519bde4411b0d462e3c31e09
DariaAleshina <ddaleshina@gmail.com>
2025-07-29 11:42:54 +0200

    mode toggle: returned the switch color

--- src/components/ModeToggle.jsx
+++ src/components/ModeToggle.jsx
@@ -3,7 +3,7 @@ function ModeToggle({ isModeFast, onModeChange }) {
     <div className="text-sm flex border-2 justify-between items-center p-0.5">
       <div className="relative z-0 flex justify-between items-center gap-1 overflow-hidden [transform-style:preserve-3d] [backface-visibility:hidden]">
         <span
-          className={`absolute bg-yellow-shadow w-1/2 h-full z-0 transition-transform duration-700 left-0 will-change-transform pointer-events-none mix-blend-multiply ${
+          className={`absolute bg-yellow-3 w-1/2 h-full z-0 transition-transform duration-700 left-0 will-change-transform pointer-events-none mix-blend-multiply ${
             isModeFast ? 'translate-x-full' : 'translate-x-0'
           }
           `}
