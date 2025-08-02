70619eab17d801be58d3d786d3a0e9b14155e436
DariaAleshina <ddaleshina@gmail.com>
2025-08-02 17:51:41 +0200

    spinner resized - even smaller dotts

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,6 +1,6 @@
 function Spinner() {
   return (
-    <span className="absolute w-12 h-12 border-5 border-dashed border-dark rounded-full inline-block animate-spin-slow"></span>
+    <span className="absolute w-12 h-12 border-4 border-dashed border-dark rounded-full inline-block animate-spin-slow"></span>
   );
 }
 
