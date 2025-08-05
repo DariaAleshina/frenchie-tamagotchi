db7160235cf995e5ea4e9b84da4cd19d25b902c3
DariaAleshina <ddaleshina@gmail.com>
2025-08-05 19:05:42 +0200

    removed preload videos effect from context

--- src/contexts/GameContext.jsx
+++ src/contexts/GameContext.jsx
@@ -40,22 +40,6 @@ function GameProvider({ children }) {
     }
   }
 
-  // preload videos
-  // useEffect(() => {
-  //   const { video } = mediaAssets;
-  //   const preloadVideo = src => {
-  //     const video = document.createElement('video');
-  //     video.src = src;
-  //     video.preload = 'auto';
-  //   };
-
-  //   preloadVideo(video.happy);
-  //   preloadVideo(video.sad);
-  //   preloadVideo(video.tired);
-  //   preloadVideo(video.normal);
-  //   preloadVideo(video.hungry);
-  // }, []);
-
   // decrease stats over time
   useEffect(() => {
     if (isGameOver || activatedAction) return;
