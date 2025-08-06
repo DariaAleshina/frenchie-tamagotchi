dc07a2ebb956725b4d4c87c8b812c1e58f50e132
DariaAleshina <ddaleshina@gmail.com>
2025-08-06 15:36:43 +0200

    state refactored with useReducer

--- src/components/ResetButton.jsx
+++ src/components/ResetButton.jsx
@@ -1,13 +1,13 @@
 import { useGame } from '../contexts/GameContext';
 
 function ResetButton() {
-  const { isGameOver, handleReset } = useGame();
+  const { isGameOver, dispatch } = useGame();
 
   const style = `cursor-pointer py-2 px-4 hover:text-yellow-dark focus:outline-none focus:ring-5 focus:ring-yellow-shadow  ${
     isGameOver && 'bg-yellow-2'
   }`;
   return (
-    <button onClick={handleReset} className={style}>
+    <button onClick={() => dispatch({ type: 'reset' })} className={style}>
       Reset the Game
     </button>
   );
